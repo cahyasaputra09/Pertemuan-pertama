6dc2d7842aa6d2239f5c4c727d2ff5473a5c7670
cahyasaputra09 <`Lenovo@DESKTOP-Q4UO40F>
2024-02-27 18:50:06 +0700

    Add project files.

---
+++ Pertemuan pertama/Pertemuan pertama.cpp
@@ -0,0 +1,21 @@
+// Pertemuan pertama.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{//begin
+	//numeric nJejari, nLuas
+    //display 'Masukan jejari= '
+	//accept nJejari
+	//compute nLuas =3.14 * r * r
+	//display 'luasnya= ' + nLuas
+
+	float nJejari, nLuas;
+	cout << "Masukan jejari";
+		cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "luasnya =" << nLuas << endl;
+  
+}//end
+
